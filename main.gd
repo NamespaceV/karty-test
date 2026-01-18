ba522fd9e43c5b2236773e6124326c7a752bf5e5
NamespaceV <s-zephyr@o2.pl>
2026-01-18 20:47:25 +0100

    shuffling cards

---
+++ main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var card_scene = load("res://Nodes/Card/card.tscn") as PackedScene
+
+func _ready() -> void:
+	$ShuffleButton.pressed.connect(shuffle)
+	shuffle()
+
+func shuffle():
+	print_debug("shuffle")
+	for c in $Cards.get_children():
+		c.queue_free()
+	for i in 10:
+		var card = card_scene.instantiate() as Card
+		card.suit = randi_range(Card.Suit.heart, Card.Suit.club)
+		card.value = randi_range(2, Card.ACE)
+		card.position = Vector2(100*(i+1), 100)
+		$Cards.add_child(card)
