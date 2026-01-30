869520bbe34e7524a65adf99e3430857d2dd3abd
PlayableGIT <piotrsalata02@gmail.com>
2026-01-30 20:46:32 +0100

    camera_mod

---
+++ camera_2d.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+var hero_pos: Vector2
+var mouse_pos: Vector2
+var mean_pos: Vector2
+@onready var player: Player = $".."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	hero_pos = player.global_position 
+	mouse_pos = get_global_mouse_position()
+	mean_pos = (hero_pos + mouse_pos)/2
+	$".".global_position = mean_pos.lerp(hero_pos, 0.02*delta)
