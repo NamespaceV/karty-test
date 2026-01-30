b37aa847aecd7272491c5dcf4e4f398a3d55f654
NamespaceV <s-zephyr@o2.pl>
2026-01-31 00:17:51 +0100

    tweak dash and shoot

--- Nodes/Player/player.gd
+++ Nodes/Player/player.gd
@@ -1,10 +1,19 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed : float = 600.0
+@export var speed : float = BASE_SPEED
 @onready var ability_1_timer: Timer = $ability_1_timer
 @onready var ability_2_timer: Timer = $ability_2_timer
+
+const BASE_SPEED = 600.0
 var ability1_on: bool
+var ability1_cooldown = 0.0
+const ABILITY_1_CD = 0.75
+const ABILITY_1_SPEED = 2400
+
+const SHOOT_CD = 0.1
+var shoot_cooldown = 0.0
+
 var ability2_on: bool
 var dash_direction: Vector2 = Vector2.ZERO
 
@@ -18,7 +27,10 @@ func _ready() -> void:
 	ability2_on = false
 	GAME.player = self
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	ability1_cooldown -= delta
+	shoot_cooldown -= delta
+
 	if ability2_on == false:
 		var i = Input.get_vector("left", "right", "up", "down")
 		velocity = i.normalized() * speed
@@ -26,14 +38,15 @@ func _process(_delta: float) -> void:
 		velocity = Vector2.ZERO
 		await get_tree().create_timer(0.3).timeout
 		velocity = dash_direction * 1000
-	
-	
-	if ability1_on:
-		speed = 1200
-	else:
-		speed = 600
 
-	if Input.is_action_just_pressed("atack"):
+
+	if ability1_on:
+		speed = ABILITY_1_SPEED
+	else:
+		speed = BASE_SPEED
+
+	if Input.is_action_pressed("atack") && shoot_cooldown < 0:
+		shoot_cooldown = SHOOT_CD
 		var mosepos = get_global_mouse_position()
 		var to_mouse = mosepos - global_position
 		var dir = to_mouse.normalized()
@@ -41,8 +54,10 @@ func _process(_delta: float) -> void:
 		bullet.global_position = self.global_position
 		bullet.set_direction(dir)
 		get_parent().add_child(bullet, true)
-		
-	if Input.is_action_just_pressed("ability1"):
+
+	if Input.is_action_just_pressed("ability1") && ability1_cooldown < 0 \
+			&& not velocity.is_zero_approx():
+		ability1_cooldown = ABILITY_1_CD
 		$ability_1_timer.start()
 		ability1_on = true
 	if Input.is_action_just_pressed("ability2"):
@@ -50,7 +65,7 @@ func _process(_delta: float) -> void:
 		var mouse_pos = get_global_mouse_position()
 		dash_direction = (mouse_pos - global_position).normalized()
 		ability2_on = true
-	
+
 	move_and_slide()
 
 
