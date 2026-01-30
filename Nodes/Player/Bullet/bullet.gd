6ed4ea67c89c3604fc183c76f20a03a75bd9ab1b
NamespaceV <s-zephyr@o2.pl>
2026-01-30 18:24:14 +0100

    shooting

---
+++ Nodes/Player/Bullet/bullet.gd
@@ -0,0 +1,20 @@
+extends AnimatableBody2D
+
+var speed = 500.0
+
+func _ready() -> void:
+	sync_to_physics = false
+
+func set_direction(dir:Vector2):
+	look_at(position+dir)
+
+
+func _process(delta: float) -> void:
+
+	var collision = move_and_collide(Vector2(1, 0).rotated(rotation) * speed * delta)
+	if collision:
+		var hit = collision.get_collider()
+		print("hit %s" % [hit])
+		queue_free()
+	if !$VisibleOnScreenNotifier2D.is_on_screen():
+		queue_free()
--- Nodes/Player/player.gd
+++ Nodes/Player/player.gd
@@ -2,7 +2,18 @@ extends CharacterBody2D
 
 @export var speed : float = 400.0
 
+var bulletScene : PackedScene = load("res://Nodes/Player/Bullet/bullet.tscn")
+
 func _process(_delta: float) -> void:
 	var i = Input.get_vector("left", "right", "up", "down")
 	velocity = i * speed
 	move_and_slide()
+
+	if Input.is_action_just_pressed("atack"):
+		var mosepos = get_global_mouse_position()
+		var to_mouse = mosepos - global_position
+		var dir = to_mouse.normalized()
+		var bullet : Node2D = bulletScene.instantiate()
+		bullet.global_position = self.global_position
+		bullet.set_direction(dir)
+		get_parent().add_child(bullet, true)
