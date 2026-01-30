f0c08bc46c0ea4b0b16ffec0305ec9beca08093c
PlayableGIT <piotrsalata02@gmail.com>
2026-01-30 23:17:26 +0100

    ability2_prototype

--- Nodes/Player/player.gd
+++ Nodes/Player/player.gd
@@ -3,7 +3,11 @@ extends CharacterBody2D
 
 @export var speed : float = 600.0
 @onready var ability_1_timer: Timer = $ability_1_timer
+@onready var ability_2_timer: Timer = $ability_2_timer
 var ability1_on: bool
+var ability2_on: bool
+var dash_direction: Vector2 = Vector2.ZERO
+
 var hasMask:bool = true
 
 var bulletScene : PackedScene = load("res://Nodes/Player/Bullet/bullet.tscn")
@@ -11,12 +15,18 @@ var maskScene : PackedScene = load("res://Nodes/Player/Mask/Mask.tscn")
 
 func _ready() -> void:
 	ability1_on = false
+	ability2_on = false
 	GAME.player = self
 
 func _process(_delta: float) -> void:
-	var i = Input.get_vector("left", "right", "up", "down")
-	velocity = i * speed
-	move_and_slide()
+	if ability2_on == false:
+		var i = Input.get_vector("left", "right", "up", "down")
+		velocity = i.normalized() * speed
+	else:
+		velocity = Vector2.ZERO
+		await get_tree().create_timer(0.3).timeout
+		velocity = dash_direction * 1000
+	
 	
 	if ability1_on:
 		speed = 1200
@@ -35,6 +45,15 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ability1"):
 		$ability_1_timer.start()
 		ability1_on = true
+	if Input.is_action_just_pressed("ability2"):
+		ability_2_timer.start()
+		var mouse_pos = get_global_mouse_position()
+		dash_direction = (mouse_pos - global_position).normalized()
+		ability2_on = true
+	
+	move_and_slide()
+
+
 
 func take_damage():
 	if not hasMask:
@@ -56,3 +75,7 @@ func wear_mask():
 
 func _on_ability_1_timer_timeout() -> void:
 	ability1_on = false
+
+
+func _on_ability_2_timer_timeout() -> void:
+	ability2_on = false
