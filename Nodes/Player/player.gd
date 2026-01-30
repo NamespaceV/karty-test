d89f3d7aafdc855a970777b0585ebc2b91ca76af
PlayableGIT <piotrsalata02@gmail.com>
2026-01-30 22:46:51 +0100

    dash_prototype_0.1

--- Nodes/Player/player.gd
+++ Nodes/Player/player.gd
@@ -1,19 +1,27 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed : float = 400.0
+@export var speed : float = 600.0
+@onready var ability_1_timer: Timer = $ability_1_timer
+var ability1_on: bool
 var hasMask:bool = true
 
 var bulletScene : PackedScene = load("res://Nodes/Player/Bullet/bullet.tscn")
 var maskScene : PackedScene = load("res://Nodes/Player/Mask/Mask.tscn")
 
 func _ready() -> void:
+	ability1_on = false
 	GAME.player = self
 
 func _process(_delta: float) -> void:
 	var i = Input.get_vector("left", "right", "up", "down")
 	velocity = i * speed
 	move_and_slide()
+	
+	if ability1_on:
+		speed = 1200
+	else:
+		speed = 600
 
 	if Input.is_action_just_pressed("atack"):
 		var mosepos = get_global_mouse_position()
@@ -23,6 +31,10 @@ func _process(_delta: float) -> void:
 		bullet.global_position = self.global_position
 		bullet.set_direction(dir)
 		get_parent().add_child(bullet, true)
+		
+	if Input.is_action_just_pressed("ability1"):
+		$ability_1_timer.start()
+		ability1_on = true
 
 func take_damage():
 	if not hasMask:
@@ -40,3 +52,7 @@ func spawn_mask():
 func wear_mask():
 	hasMask = true
 	$AnimationPlayer.play("RESET")
+
+
+func _on_ability_1_timer_timeout() -> void:
+	ability1_on = false
