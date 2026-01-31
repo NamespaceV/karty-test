a95ff9726268c57290fe7318c8a6cabb51a62e24
NamespaceV <s-zephyr@o2.pl>
2026-01-31 22:17:39 +0100

    settings menu

--- hud.gd
+++ hud.gd
@@ -6,3 +6,8 @@ func _process(_delta: float) -> void:
 	$Stamina.value = GAME.player.stamina
 	$BossHp.max_value = GAME.boss.MAX_HP
 	$BossHp.value = GAME.boss.boss_hp
+
+
+func _on_settings_button_pressed() -> void:
+	$"Settings Menu".visible = not $"Settings Menu".visible
+	get_tree().paused = $"Settings Menu".visible
--- settings_menu.gd
+++ settings_menu.gd
@@ -11,11 +11,11 @@ func _process(_delta):
 	volume_update()
 
 func volume_update():
-	var master_volume = $master_soundbar
+	var master_volume = $VBoxContainer/master_soundbar
 	AudioGlobal.master_volume = master_volume.value
-	var music_volume = $master_soundbar/music_soundbar
+	var music_volume = $VBoxContainer/music_soundbar
 	AudioGlobal.music_volume = music_volume.value
-	var sfx_volume = $master_soundbar/sfx_soundbar
+	var sfx_volume = $VBoxContainer/sfx_soundbar
 	AudioGlobal.sfx_volume = sfx_volume.value
-	var ambience_volume = $master_soundbar/ambience_soundbar
+	var ambience_volume = $VBoxContainer/ambience_soundbar
 	AudioGlobal.ambience_volume = ambience_volume.value
