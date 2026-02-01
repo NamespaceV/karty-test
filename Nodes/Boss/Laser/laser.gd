a322e626c3b3f8612fc4aeed5257fa63cc5c7431
NamespaceV <s-zephyr@o2.pl>
2026-02-01 04:05:57 +0100

    fix lasers, player floats

--- Nodes/Boss/Laser/laser.gd
+++ Nodes/Boss/Laser/laser.gd
@@ -1,23 +1,19 @@
 extends Node2D
 
 var speed = 1000
+const LENGTH = 2600.0
 
 func _ready() -> void:
 	var tween = get_tree().create_tween()
 	scale = Vector2(0,1)
-	tween.tween_property($".", "scale", Vector2.ONE, $ColorRect.size.y/speed)
-	pass # Replace with function body.
+	tween.tween_property(self, "scale", Vector2.ONE,LENGTH/speed)
 
 
 func _process(delta: float) -> void:
 	position += Vector2(1.0, 0.0).rotated(rotation) * speed * delta
 
 
-func _on_laser_area_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("hero"):
 		var p = body as Player
 		p.take_damage()
-
-
-func _on_laser_area_body_exited(_body: Node2D) -> void:
-	pass # Replace with function body.
