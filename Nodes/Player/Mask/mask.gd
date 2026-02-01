f7f83ced2ad7124ad9d65777b15d8271b17eb43d
NamespaceV <s-zephyr@o2.pl>
2026-02-01 09:54:27 +0100

    add particle on mask drop

--- Nodes/Player/Mask/mask.gd
+++ Nodes/Player/Mask/mask.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+var hitPfxScene = load("res://Nodes/Player/MastThrow/MaskThrowPfx.tscn")
+const FLY_TIME = 0.2
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("hero"):
@@ -10,4 +13,13 @@ func _on_body_entered(body: Node2D) -> void:
 func from_player(pos : Vector2):
 	$Sprite2D.global_position = pos
 	var tween = get_tree().create_tween()
-	tween.tween_property($Sprite2D, "position", Vector2.ZERO, 0.2)
+	tween.tween_property($Sprite2D, "position", Vector2.ZERO, FLY_TIME)
+	get_tree().create_timer(FLY_TIME).timeout.connect(play_hit_particle)
+
+
+func play_hit_particle():
+	var n = hitPfxScene.instantiate() as CPUParticles2D
+	n.position = global_position
+	get_parent().add_child(n)
+	n.emitting = true
+	n.finished.connect(func():n.queue_free())
